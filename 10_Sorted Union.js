f92bf4af42d1f96abaa776f3cff4fc17d479c272
Capucine25 <40346652+Capucine25@users.noreply.github.com>
2018-06-19 13:25:26 -0400

    Update 10_Sorted Union.js

--- 10_Sorted Union.js
+++ 10_Sorted Union.js
@@ -1,15 +1,13 @@
 function uniteUnique(arr) {
 
-arr=arr.concat(arguments[1]).concat(arguments[2]).concat(arguments[3])
+for (var i = 0; i < arguments.length; i++) {
+    arr=arr.concat(arguments[i])}
+
 var arr2 = arr.slice(0)
 
   for (var i=0; i<arr.length;i++){
     if (arr2.indexOf(arr[i]) != arr2.lastIndexOf(arr[i])){
-      arr2.splice(arr2.lastIndexOf(arr[i]),1)
-      
-    }
-    else if (arr[i] == null)
-      {arr2.splice(arr2.indexOf(arr[i]),1)}
+      arr2.splice(arr2.lastIndexOf(arr[i]),1)}
     }
 return arr2;   
 }
