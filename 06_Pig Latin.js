312f0edfbc610a8a3ef8ebe53d54d44a8fd01fff
Capucine25 <40346652+Capucine25@users.noreply.github.com>
2018-06-19 09:41:37 -0400

    Update 06_Pig Latin.js

--- 06_Pig Latin.js
+++ 06_Pig Latin.js
@@ -1,11 +1,14 @@
 function translatePigLatin(str) {
   
+  //if there is only consonants in the word
   if(/^[^aeiou]+$/.test(str))
     {return str+"ay"}
     
+ //Word starts with a consonant
   else if (/^[^aeiou]+/.test(str))
     {return str.replace(/(^[^aeiou]+)([A-Z]+)/i, '$2$1')+"ay";}
   
+  //Word starts with a vowel
   else
     {return str+"way"}
 
