0d93e868e1b219037e83ddda6f5d93fc4c916e35
Capucine25 <40346652+Capucine25@users.noreply.github.com>
2018-06-20 15:15:33 -0400

    Update 16_Sum All Primes.js

--- 16_Sum All Primes.js
+++ 16_Sum All Primes.js
@@ -20,3 +20,32 @@ var primeNum=2;
      
   return primeNum;
 }
+
+//Alternative solution that should take less time
+
+function sumPrimes(num) {
+  
+var Sum=2;
+var primeNum=[2]
+
+//Checks for every number lower or equals to num
+  for (var i=3;i<=num;i++){
+   var test=0;
+   
+   //Check if the number is prime (is if divisible by any other prime?)
+   //If it is, adds one to test value. If test value stays to 0, the number is prime
+   //and is added to the sum (value Sum) and to the array containing all prime.
+    for (var j=0;j<primeNum.length;j++){
+      if (i%primeNum[j]==0)
+        {test+=1;}
+    }
+
+    if (test ==0)
+      {Sum+=i;
+      primeNum.push(i) 
+      }
+    
+  }  
+     
+  return Sum;
+}
